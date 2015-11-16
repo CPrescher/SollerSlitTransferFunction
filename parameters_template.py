9f20d2403a677fe6812f547234863856a004d4ac
Clemens Prescher <clemens.prescher@gmail.com>
2015-11-16 16:30:33 +0100

    Nothing fancy, just a template

---
+++ parameters_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+__author__ = 'Clemens Prescher'
+
+# This file is the template for your own parameters.py file which should be created before running anything...
+
+# Parameters in mm:
+r1 = 62 # defines the radius to the inner slit set
+r2 = 210 # defines the radius to the outer slit set
+w1 = 0.025 # the width of one of the inner slits
+w2 = 0.025 # the width of one of the outer slits
+b1 = 8 # length of the inner slits
+b2 = 6# length of the outer slits
+
+example_thickness = 0.05
